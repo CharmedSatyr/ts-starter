34a732c0f2b6570aac77e9a2836471cafdcc233c
Joseph <z@charmed.tech>
2019-05-06 15:15:13 -0700

    Tweak line width to 80

--- src/tacker.ts
+++ src/tacker.ts
@@ -1,28 +1,32 @@
-import { name } from 'faker'
+import { name } from 'faker';
 
 class Student {
-  public fullName: string // Must define here and in constructor. Makes `fullName` only accessible from `yes()`.
+  // Must define here and in constructor.
+  // Makes `fullName` only accessible from `yes()`.
+  public fullName: string;
   constructor(
     public firstName: string,
     public middleInitial: string,
     public lastName: string
   ) {
-    // Using `public` arguments creates `this.firstName = firstName;`, etc., in the js
-    this.fullName = `${firstName} ${middleInitial && middleInitial} ${lastName}`
+    // Using `public` arguments creates
+    // `this.firstName = firstName;`, etc., in the js
+    this.fullName = `${firstName} ${middleInitial &&
+      middleInitial} ${lastName}`;
   }
   public yes(): string {
-    return this.fullName
+    return this.fullName;
   }
 }
 
 interface IPerson {
-  firstName: string
-  lastName: string
+  firstName: string;
+  lastName: string;
 }
 
 const greeter = (person: IPerson) =>
-  `hello ${person.firstName} ${person.lastName}!`
+  `hello ${person.firstName} ${person.lastName}!`;
 
-const user = new Student(name.firstName(), name.firstName(), name.lastName())
+const user = new Student(name.firstName(), name.firstName(), name.lastName());
 
-// console.log(user.yes())
+export default Student;
